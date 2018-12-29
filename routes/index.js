ebeb2a97659753ea3f103e69f715746415d46831
SiebeGerritsen <33697618+SiebeGerritsen@users.noreply.github.com>
2018-12-29 20:49:57 +0100

    update

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let router = express.Router();
+const request = require('request');
+
+const apiKey = '0de153a6e7924982ab8b6a2131983a2d';
+
+/* GET home page. */
+router.get('/~', function (req, res, next) {
+    error = req.query.error;
+    res.render('index', {title: 'WeatherApp', error: error});
+});
+
+router.get('/weather', function (req, res, next) {
+
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+    request(url, function (err, response, body) {
+        if(err){
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            let weather = JSON.parse(body)
+            if(weather.main == undefined){
+                res.render('index', {weather: null, error: 'Error, please try again'});
+            } else {
+
+
+                const knots  = Math.round(weather.wind.speed * 1.944).toFixed(1);
+                const temp = Math.round(weather.main.temp);
+
+                let weatherText= `It's ${temp} degrees Celcius in ${weather.name}!`;
+                let weartherWind= `The wind speed in ${knots} knots !`;
+
+                res.render('index', {weather: weatherText, test: weartherWind, temp: temp, error: null});
+            }
+        }
+    });
+});
+
+module.exports = router;
