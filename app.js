bf4d1c2a98ea846d83ecbdf9f2a3c082280b0a1f
SiebeGerritsen <33697618+SiebeGerritsen@users.noreply.github.com>
2018-12-29 20:47:52 +0100

    update

--- app.js
+++ app.js
@@ -3,33 +3,49 @@ const createError = require('http-errors');
 const path = require('path');
 const cookieParser =  require('cookie-parser');
 const logger = require('morgan');
-
 const express = require('express');
-const request = require('request');
 const app = express();
 const bodyParser = require('body-parser');
-const apiKey = '0de153a6e7924982ab8b6a2131983a2d';
 
+const indexRouter = require('./routes/index');
+
+//view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(express.static('public'));
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({extended: true}));
 
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+
+
+
+/*
 //rendert de index pagina
 app.get('/', function (req, res) {
     res.render('index', {weather: null, error: null});
@@ -60,27 +76,9 @@ app.post('/', function (req, res) {
             }
         }
     });
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-module.exports = app;
-
-
-
-/*
-//poort where site is listing
-app.listen(8888, function () {
-    console.log('Example app listening on port 8888!')
-});
+})
 
 */
+
+//poort where site is listing
+app.listen(10333);
